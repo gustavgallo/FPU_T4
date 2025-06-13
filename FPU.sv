f5cc9cf50ccb0c7190585e09e4b906c6e0d3589c
Gustavo Gallo <gustavotibollagallo@gmail.com>
2025-06-13 09:24:18 -0300

    a lot

--- FPU.sv
+++ FPU.sv
@@ -27,47 +27,92 @@ logic output [3:0] status_out;
 
 typedef enum logic [1:0]{  
     
-    DECODE,
+    PRE_SUM,
 
-    CALC
+    SUM,
+
+    AJUST
 
 } state_t;
 
 state_t EA;
-logic [31:0] decoded_A, decoded_B;
-logic [5:0] exp_A, exp_B;
+logic [5:0] exp_A, exp_B, exp_diff;
+logic signal [24:0] Mantissa_A, Mantissa_B;
+logic signal_result; 
+logic greater; // A = 0, B = 1
+logic start = 1;
+logic PRE_done = 0;
+
+
+
+
+
 
 always_ff @(posedge clock, negedge reset)begin
 
     if(!reset)begin
     data_out <= 0;
     status_out<=0;
+    start <= 1;
+    PRE_done = 0;
     
     end else begin
         
         case(EA)
 
-            DECODE:begin
-                if(!op_A_in)begin
-                decoded_A <=
-                end else begin
-                decoded_A <=
+            PRE_SUM:begin
+                if(start)begin
+                    Mantissa_A <= {1, op_A_in[24:0]};
+                    Mantissa_B <= {1, op_B_in[24:0]};
+                    exp_A <= op_A_in[30:25];
+                    exp_B <= op_B_in[30:25];
+                    start <= 0;
 
+                    if(op_A_in[30:25] >= op_B_in[30:25])begin
+                        exp_diff <= (op_A_in[30:25] - op_B_in[30:25]);
+                        signal_result <= op_A_in[31];
+                        greater <= 0;
+                
+                    end else begin
+
+                        exp_diff <= (op_B_in[30:25] - op_A_in[30:25]);
+                        signal_result <= op_B_in[31];
+                        greater <= 1;
+                    end
+
+                    end else begin
+                        if(!PRE_done)begin
+                            if(!greater)begin // if A is greater than
+                                Mantissa_B <= Mantissa_B >> exp_diff;
+                            end else begin // if B is greater
+
+                                Mantissa_A <= Mantissa_A >> exp_diff;
+
+                            end
+                            PRE_done <= 1;
+                        end    
+                    end// end do else
+            end
+
+            AJUST:begin
+                if(op_A_in[31])begin
+                    Mantissa_A <= -Mantissa_A;
                 end
 
-                if(!op_B_in)begin
-                decoded_B <=
-                end else begin
-                decoded_B <=
-
+                if(op_B_in[31])begin
+                    Mantissa_B <= -Mantissa_B;
                 end
 
             end
 
-            CALC:begin
+            SUM:begin
+
+                
+
 
 
             end
+            
 
 
 
@@ -81,21 +126,30 @@ end
 always_ff @(posedge clock, negedge reset)begin
 
     if(!reset)begin
-    EA <= DECODE;
+    EA <= PRE_SUM;
     
     end else begin
         
         case(EA)
 
-            DECODE:begin
+            PRE_SUM:begin
+                if(PRE_done) EA <= AJUST;
+                else EA <= PRE_SUM;
+
+            end
+
+            AJUST:begin
+
 
 
             end
 
-            CALC:begin
+            SUM:begin
 
 
             end
+
+            
 
 
 
